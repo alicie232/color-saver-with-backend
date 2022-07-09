3f07ed8e691d5a55bcf83a1e77eb4e2487fb374e
Alice Cieslik <alice.cieslik@gmail.com>
2022-07-09 20:34:04 +0200

    implement route login

--- api/users/login.js
+++ api/users/login.js
@@ -1,1 +1,15 @@
+import connectToMongodb from "../_database/connect-to-mongodb";
+import { User } from "../_database/models/index.js";
 
+const handler = async (request, response) => {
+  try {
+    await connectToMongodb();
+    const user = await User.findOne({
+      nickName: request.body.nickName,
+    });
+    return response.json(user);
+  } catch (error) {
+    return response.status(500).json({ message: "Server Error" });
+  }
+};
+export default handler;
