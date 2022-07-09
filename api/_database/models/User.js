f36051032cbab347b124b85fe153ea3396f38920
Alice Cieslik <alice.cieslik@gmail.com>
2022-07-09 15:02:18 +0200

    define User model

---
+++ api/_database/models/User.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+export const UserSchema = new mongoose.Schema({
+  nickName: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+});
